b095248ffce42bafb78de8fd1788ebd19e67c1cc
Nguyen Thanh Trung <thanhtrung160204@gmail.com>
2024-08-12 17:33:21 +0700

    fix: name migrations refresh and manufacturer

--- server/src/migrations/create-manuFacturer.js
+++ server/src/migrations/create-manuFacturer.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("manuFacturer", {
+    await queryInterface.createTable("manufacturer", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -26,6 +26,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("manuFacturer");
+    await queryInterface.dropTable("manufacturer");
   },
 };
