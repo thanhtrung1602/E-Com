c98db401a80e48cf3aac5c37a45b988c2d268cad
Dev <hoangtv1809@gmail.com>
2024-08-14 00:21:25 +0700

    feat: created login

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,8 +1,31 @@
-import { FaEye } from "react-icons/fa";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { FaEyeSlash } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import usePost from "~/api/post";
 import loginImage from "~/assets/loginImage.png";
 
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const { mutate: login } = usePost();
+
+
+
+    const handleLogin = () => {
+        const data = {
+            email,
+            password
+        }
+        login({ url: "/auth/login", data }, {
+            onSuccess: (data) => {
+                if (data.status === 200) {
+                    toast.success("Đăng ký thành công")
+                }
+            }
+        })
+    }
+
     return (
         <div className="flex h-screen">
             <div className="">
@@ -12,13 +35,23 @@ function Login() {
                 <div className="w-[456px]">
                     <h1 className="pb-6 text-black text-3xl font-bold">Sign up</h1>
                     <p className="pb-8 text-black">Don’t have an accout yet? <span className="font-semibold text-main500">Sign Up</span></p>
-                    <form action="">
+                    <form onSubmit={handleLogin}>
                         <div className="pb-8">
-                            <input type="text" placeholder="Email address" className="bg-transparent text-black w-full outline-none border-b-2 pb-1" />
+                            <input
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                type="text"
+                                placeholder="Email address"
+                                className="bg-transparent text-black w-full outline-none border-b-2 pb-1" />
                         </div>
                         <div className="relative">
-                            <input type="password" placeholder="Password" className="bg-transparent text-black w-full outline-none border-b-2 pb-1" />
-                            <FaEye className="absolute right-1 bottom-1 text-[#D1D4DB]  " />
+                            <input
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                type="password"
+                                placeholder="Password"
+                                className="bg-transparent text-black w-full outline-none border-b-2 pb-1" />
+                            <FaEyeSlash className="absolute right-1 bottom-1 text-[#D1D4DB]" />
                         </div>
                         <div className="py-8 flex items-center justify-between">
                             <div className="flex items-center gap-x-2">
@@ -29,7 +62,9 @@ function Login() {
                                 <Link to={"/forgotpass"} className="font-semibold text-black">Forgot password</Link>
                             </div>
                         </div>
-                        <button className="w-full py-2 bg-main500 rounded">
+                        <button
+                            type="submit"
+                            className="w-full py-2 bg-main500 rounded">
                             Sign In
                         </button>
                     </form>
