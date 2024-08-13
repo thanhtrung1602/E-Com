9fb4d31463fb580dae8d767ec251efd2d7b5bffd
Nguyen Thanh Trung <thanhtrung160204@gmail.com>
2024-08-14 00:44:18 +0700

    test: test login

--- client/src/api/post.tsx
+++ client/src/api/post.tsx
@@ -4,13 +4,7 @@ import instance from "~/services/axios";
 function usePost() {
   return useMutation({
     mutationFn: ({ url, data }: { url: string; data: object }) =>
-      instance
-        .post(url, data, {
-          headers: {
-            Authorization: `Bearer `,
-          },
-        })
-        .then((response) => response.data),
+      instance.post(url, data).then((response) => response),
   });
 }
 
--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import toast from "react-hot-toast";
 import { FaEyeSlash } from "react-icons/fa";
 import { Link } from "react-router-dom";
@@ -6,72 +6,89 @@ import usePost from "~/api/post";
 import loginImage from "~/assets/loginImage.png";
 
 function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const { mutate: login } = usePost();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { mutate: login } = usePost();
 
-
-
-    const handleLogin = () => {
-        const data = {
-            email,
-            password
-        }
-        login({ url: "/auth/login", data }, {
-            onSuccess: (data) => {
-                if (data.status === 200) {
-                    toast.success("Đăng ký thành công")
-                }
-            }
-        })
-    }
-
-    return (
-        <div className="flex h-screen">
-            <div className="">
-                <img src={loginImage} alt="" className="h-full w-full" />
-            </div>
-            <div className="bg-white flex items-center justify-center py-28 px-20 w-1/2">
-                <div className="w-[456px]">
-                    <h1 className="pb-6 text-black text-3xl font-bold">Sign up</h1>
-                    <p className="pb-8 text-black">Don’t have an accout yet? <span className="font-semibold text-main500">Sign Up</span></p>
-                    <form onSubmit={handleLogin}>
-                        <div className="pb-8">
-                            <input
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                type="text"
-                                placeholder="Email address"
-                                className="bg-transparent text-black w-full outline-none border-b-2 pb-1" />
-                        </div>
-                        <div className="relative">
-                            <input
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                type="password"
-                                placeholder="Password"
-                                className="bg-transparent text-black w-full outline-none border-b-2 pb-1" />
-                            <FaEyeSlash className="absolute right-1 bottom-1 text-[#D1D4DB]" />
-                        </div>
-                        <div className="py-8 flex items-center justify-between">
-                            <div className="flex items-center gap-x-2">
-                                <input type="checkbox" name="" id="checkBox" className="size-5 text-white accent-main500" />
-                                <label htmlFor="checkBox" className="text-black">Remember me</label>
-                            </div>
-                            <div className="">
-                                <Link to={"/forgotpass"} className="font-semibold text-black">Forgot password</Link>
-                            </div>
-                        </div>
-                        <button
-                            type="submit"
-                            className="w-full py-2 bg-main500 rounded">
-                            Sign In
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </div>
+  const handleLogin = (e: FormEvent) => {
+    e.preventDefault();
+    const data = {
+      email,
+      password,
+    };
+    login(
+      { url: "/auth/login", data },
+      {
+        onSuccess: (data) => {
+          if (data) {
+            toast.success("Đăng ký thành công");
+          }
+        },
+        onError: (error) => {
+          console.log(error);
+        },
+      }
     );
+  };
+
+  return (
+    <div className="flex h-screen">
+      <div className="">
+        <img src={loginImage} alt="" className="h-full w-full" />
+      </div>
+      <div className="bg-white flex items-center justify-center py-28 px-20 w-1/2">
+        <div className="w-[456px]">
+          <h1 className="pb-6 text-black text-3xl font-bold">Sign up</h1>
+          <p className="pb-8 text-black">
+            Don’t have an accout yet?{" "}
+            <span className="font-semibold text-main500">Sign Up</span>
+          </p>
+          <form onSubmit={handleLogin}>
+            <div className="pb-8">
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="text"
+                placeholder="Email address"
+                className="bg-transparent text-black w-full outline-none border-b-2 pb-1"
+              />
+            </div>
+            <div className="relative">
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                placeholder="Password"
+                className="bg-transparent text-black w-full outline-none border-b-2 pb-1"
+              />
+              <FaEyeSlash className="absolute right-1 bottom-1 text-[#D1D4DB]" />
+            </div>
+            <div className="py-8 flex items-center justify-between">
+              <div className="flex items-center gap-x-2">
+                <input
+                  type="checkbox"
+                  name=""
+                  id="checkBox"
+                  className="size-5 text-white accent-main500"
+                />
+                <label htmlFor="checkBox" className="text-black">
+                  Remember me
+                </label>
+              </div>
+              <div className="">
+                <Link to={"/forgotpass"} className="font-semibold text-black">
+                  Forgot password
+                </Link>
+              </div>
+            </div>
+            <button type="submit" className="w-full py-2 bg-main500 rounded">
+              Sign In
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
