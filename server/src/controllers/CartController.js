3654dfd3841c0c0f45566be09d22ce285573def6
Nguyen Thanh Trung <thanhtrung160204@gmail.com>
2024-08-15 01:29:29 +0700

    fix: check error cart

--- server/src/controllers/CartController.js
+++ server/src/controllers/CartController.js
@@ -4,14 +4,30 @@ class CartController {
   async createCart(req, res) {
     const { userId, productId, quantity, total } = req.body;
     try {
-      if (!userId || !productId || typeof quantity !== "number" || typeof total !== "number") {
+      console.log("product >>>>", productId);
+      console.log("userId >>>>", userId);
+      console.log(
+        "quatity>>>>>>>>",
+        quantity,
+        "quantityCheck>>>>>>>",
+        typeof quantity
+      );
+      console.log("total>>>>>>>>", total, "quantityCheck>>>>>>>", typeof total);
+      if (
+        !userId ||
+        !productId ||
+        typeof Number(quantity) !== "number" ||
+        typeof Number(total) !== "number"
+      ) {
         return res.status(400).json("Invalid input");
       }
 
       const createCart = await cartService.createCart(req.body);
       return res.status(200).json(createCart);
     } catch (error) {
-      return res.status(500).json({ error: "An error occurred", details: error.message });
+      return res
+        .status(500)
+        .json({ error: "An error occurred", details: error.message });
     }
   }
 }
